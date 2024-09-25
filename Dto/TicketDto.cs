6bd66df075929ddd88b50ef67e788945f89fa6e0
Sailakshmi Navaneetam <sailakshmi.navaneetam@bouvet.no>
2024-09-25 10:28:18 +0200

    updated with development

--- Dto/TicketDto.cs
+++ Dto/TicketDto.cs
@@ -5,5 +5,6 @@
         public string Title { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
---
+++ Migrations/20240925082103_running after merged with development 2.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticketron.Migrations
+{
+    /// <inheritdoc />
+    public partial class runningaftermergedwithdevelopment2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Participants_ParticipantId",
+                table: "Tickets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParticipantId",
+                table: "Tickets",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Participants_ParticipantId",
+                table: "Tickets",
+                column: "ParticipantId",
+                principalTable: "Participants",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Participants_ParticipantId",
+                table: "Tickets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParticipantId",
+                table: "Tickets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Participants_ParticipantId",
+                table: "Tickets",
+                column: "ParticipantId",
+                principalTable: "Participants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
